968dec86aad3481b6fd60b5551f1d4aba1e864cf
JonathanCruzL <100337650+JonathanCruzL@users.noreply.github.com>
2023-03-09 11:50:59 -0500

    Update app_gpt.py

--- app_gpt.py
+++ app_gpt.py
@@ -6,14 +6,8 @@ from streamlit_webrtc import webrtc_streamer as webStreamer
 def app():
     st.title("Aplicación de cámara en tiempo real")
 
-    webrtc_streamer = webStreamer(
-        key="camera",
-        type="video",
-        source=webrtc.VideoTransformerBase(
-            fps=24,
-            transform_func=lambda frame: cv2.cvtColor(frame, cv2.COLOR_BGR2RGB),
-        ),
-    )
+    webrtc_streamer = webStreamer(key="camera", type="video",
+                                  source=webrtc.VideoTransformerBase(fps=24,transform_func=lambda frame: cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)))
 
     if not webrtc_streamer:
         st.warning("No se ha podido acceder a la cámara.")
