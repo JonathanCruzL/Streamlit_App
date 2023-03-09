cfc9411757fcd2ce7d1fef9b4f2f0ccb0a67c716
JonathanCruzL <100337650+JonathanCruzL@users.noreply.github.com>
2023-03-09 12:04:01 -0500

    Update app_gpt.py

--- app_gpt.py
+++ app_gpt.py
@@ -1,20 +1,51 @@
-import cv2
+import threading
+from typing import Union
+
+import av
+import numpy as np
 import streamlit as st
-import streamlit_webrtc as webrtc
-from streamlit_webrtc import webrtc_streamer as webStreamer
 
-def app():
-    st.title("Aplicación de cámara en tiempo real")
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 
-    webrtc_streamer = webStreamer(key="camera", type="video",
-                                  source=webrtc.VideoTransformerBase(fps=24,transform_func=lambda frame: cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)))
 
-    if not webrtc_streamer:
-        st.warning("No se ha podido acceder a la cámara.")
-        return
+def main():
+    class VideoTransformer(VideoTransformerBase):
+        frame_lock: threading.Lock  # `transform()` is running in another thread, then a lock object is used here for thread-safety.
+        in_image: Union[np.ndarray, None]
+        out_image: Union[np.ndarray, None]
 
-    image = webrtc_streamer.image_in_color
-    st.image(image, use_column_width=True)
+        def __init__(self) -> None:
+            self.frame_lock = threading.Lock()
+            self.in_image = None
+            self.out_image = None
+
+        def transform(self, frame: av.VideoFrame) -> np.ndarray:
+            in_image = frame.to_ndarray(format="bgr24")
+
+            out_image = in_image[:, ::-1, :]  # Simple flipping for example.
+
+            with self.frame_lock:
+                self.in_image = in_image
+                self.out_image = out_image
+
+            return out_image
+
+    ctx = webrtc_streamer(key="snapshot", video_transformer_factory=VideoTransformer)
+
+    if ctx.video_transformer:
+        if st.button("Snapshot"):
+            with ctx.video_transformer.frame_lock:
+                in_image = ctx.video_transformer.in_image
+                out_image = ctx.video_transformer.out_image
+
+            if in_image is not None and out_image is not None:
+                st.write("Input image:")
+                st.image(in_image, channels="BGR")
+                st.write("Output image:")
+                st.image(out_image, channels="BGR")
+            else:
+                st.warning("No frames available yet.")
+
 
 if __name__ == "__main__":
-    app()
+    main()
